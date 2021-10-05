3df8e7c1cb763b9fc3363fb3af703a5c161f39d6
Andy Lam <andylammm42@gmail.com>
2021-10-06 00:47:57 +0100

    writing to csv

--- product.py
+++ product.py
@@ -6,3 +6,8 @@ while True:
 	price = input('Please enter the price: ')
 	products.append([name, price])
 print(products) 
+
+with open('product.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
